377ad64eaf67aae0e7d3fb0537ba5ba511504ed6
Haven Smith <haven@Havens-MacBook-Air.local>
2021-03-07 12:41:02 -0600

    add method to count leading 1s in a byte

--- project.c
+++ project.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+int byteLength(char leadByte){
+   int count = 0;
+   int i = 7;
+   while(((leadByte >> i) & 0x01) == 0x01) {
+      count++;
+      i--;
+   }
+   return count;
+}
+
+
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+   char c;
+   c = getchar();
+   printf("Character Entered: ");
+   putchar(c);
+
+   printf("\n%d", byteLength(c));
+   // for(int i = 7; i >= 0; i--) {
+   //    printf("%d\n", (c >> i) & 0x01);
+   // }
+   
+
    return 0;
 }
+
+
