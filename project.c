dba340faf0ea7fc56e068dc8c95fe2ccb8548fda
Haven Smith <haven@Havens-MacBook-Air.local>
2021-03-07 20:08:23 -0600

    add method to output binary

--- project.c
+++ project.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int byteLength(char leadByte){
+int leadingOneCount(char leadByte){
    int count = 0;
    int i = 7;
    while(((leadByte >> i) & 0x01) == 0x01) {
@@ -9,19 +9,44 @@ int byteLength(char leadByte){
    return count;
 }
 
+int utf(char bytes[], int byteLength) {
+   // for(int i = 0; i < 4; i++) {
+   //    char byte = bytes[i];
+   //    for (int j = 0; j < 8; j++) {
+   //       printf("%d", !!((byte << j) & 0x80));
+   //    }
+   //    printf("\n");
+   // }
+   // return 0;
+   if(byteLength == 0){
+      return 
+   }
+   else{
+      for(int i = byteLength - 1; i <= 0; i--) {
+
+      }
+   }
+   
+   
+}
+
 
 
 int main() {
-   char c;
-   c = getchar();
+   char c = getchar();
+   while(c != EOF){
+      char bytes[4] = {0, 0, 0, 0};
+      int byteLength = leadingOneCount(c);
+      bytes[0] = c;
+      for(int i = 1; i < byteLength; i++){
+         bytes[i] = getchar();
+      }
+      utf(bytes, byteLength);
+      c = getchar();
+   }
    printf("Character Entered: ");
    putchar(c);
-
-   printf("\n%d", byteLength(c));
-   // for(int i = 7; i >= 0; i--) {
-   //    printf("%d\n", (c >> i) & 0x01);
-   // }
-   
+   printf("\n");
 
    return 0;
 }
